14918396ab0159bc69b2b03751c171e4a418362d
Oleg Bovykin <oleg.bovykin@gmail.com>
2015-05-20 14:23:51 +0300

    Updated tests to connect to real heroku centrifuge

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ require 'bundler/setup'
 Bundler.setup
 require 'centrifuge'
 require 'rspec'
-require 'webmock/rspec'
+# require 'webmock/rspec'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
@@ -43,10 +43,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.before(:each) do
-    WebMock.reset!
-    WebMock.disable_net_connect!
-  end
+  # config.before(:each) do
+  #   WebMock.reset!
+  #   WebMock.disable_net_connect!
+  # end
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
